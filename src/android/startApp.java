5fc3644e20d450ed3726168549a73d535557a9a1
Fanie Oosthuysen <fanie.oosthuysen@gmail.com>
2016-08-24 00:53:34 +0200

    Missing Imports

--- src/android/startApp.java
+++ src/android/startApp.java
@@ -7,6 +7,8 @@
 */
 package com.lampa.startapp;
 
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
