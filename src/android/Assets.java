fa4f8c778881cf25ef883c7b4a2c4c2442c89029
gnhaku <hello@gnhaku.me>
2020-08-11 17:11:12 +0300

    Fix missed import

--- src/android/Assets.java
+++ src/android/Assets.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import org.apache.cordova.CordovaPlugin;
 
 import java.lang.reflect.Field;
+import java.util.regex.Pattern;
 
 
 public class Assets extends CordovaPlugin {
